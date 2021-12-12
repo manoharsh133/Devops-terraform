24672d9ca5649947d3e4b085e06b24c8696862c5
Manohar <72411177+manoharsh133@users.noreply.github.com>
2021-12-12 23:20:20 +0530

    updated
    updated

--- vars.tf
+++ vars.tf
@@ -1,13 +1,14 @@
+# Access Key
+variable AWS_ACCESSKEY {
+  default     = "AKIAZNCDVTPMCUMDIHVV"
+}
+
+# Secret Key
+variable AWS_SECRETKEY {
+  default     = "xFo1+8SRHtVkGymxp4UDfsGzVBaDOpuwgZshGTwH"
+}
+
 # Region
 variable AWS_REGION {
   default     = "ap-south-1"
 }
-
-# Image
-variable instance_data {
-  default = {
-    count = "5"
-    ami = "ami-0d1a4d53e40abecc4"
-    type = "t2.micro"
-  }
-
